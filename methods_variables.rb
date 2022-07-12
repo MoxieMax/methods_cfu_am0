069fb96c08f137254bad59303df32a42077464e6
Max MacGillivray <108768349+MoxieMax@users.noreply.github.com>
2022-07-12 16:25:07 -0700

    Update methods_variables.rb

--- methods_variables.rb
+++ methods_variables.rb
@@ -8,11 +8,11 @@
 #  call reverse on the variable, print it out
 #  call length on the variable, print it out
 
-
-
-
-
-
+name = "Daisy"
+puts name.upcase
+puts name.downcase
+puts name.reverse
+puts name.length
 
 # =================================
 # PART 2
@@ -23,10 +23,16 @@ last_login = "12/09/2021"
 # 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do. 
   # Write the methods out on the lines below, with an explanation in your own words of how they work.
 
-
+puts user_name.capitalize!
+puts last_login.size
+puts user_name.prepend("Hello ")
+puts user_name.count("a-z")
 
 # 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
 
+#`!` at the end of the method name will make the changes permanent, and putting methods after will apply to the changed item
 
-
+name = "Daisy"
+puts name.reverse!
+puts name.capitalize
 
